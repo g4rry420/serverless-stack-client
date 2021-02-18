0d1e5262f6ba1ef8758b1384e62ebff738670147
gurkiran_singh <gurkiransinghk@gmail.com>
2021-02-18 15:37:13 -0500

    added environment variables and also added  forgot password implementation for user

--- src/config.js
+++ src/config.js
@@ -1,19 +1,19 @@
 const config = {
     MAX_ATTACHMENT_SIZE: 5000000,
-    STRIPE_KEY: "pk_test_51H0FNhKAONAeMcm84S1EmYaex9AG4c56r8fWM1HNRMHTHDlvWCqYjiyy7QUrzRBEzZwvJyS3UHM6i9N5Cut4LXm500ZBfo02rx",
+    STRIPE_KEY: process.env.REACT_APP_STRIPE_KEY,
     s3: {
       REGION: "ca-central-1",
-      BUCKET: "notesapp.upload",
+      BUCKET: "notesapp.upload"
     },
     apiGateway: {
       REGION: "ca-central-1",
-      URL: "https://rghoz0au9l.execute-api.ca-central-1.amazonaws.com/prod",
+      URL: process.env.REACT_APP_API_GATEWAY_URL
     },
     cognito: {
       REGION: "ca-central-1",
-      USER_POOL_ID: "ca-central-1_5eKrgzUw7",
-      APP_CLIENT_ID: "2bjsfludoapkg1hdeq7qvfkakp",
-      IDENTITY_POOL_ID: "ca-central-1:e9dd777c-5f62-425a-a3d9-30d4c1f06a0d",
+      USER_POOL_ID: process.env.REACT_APP_USER_POOL_ID,
+      APP_CLIENT_ID: process.env.REACT_APP_APP_CLIENT_ID,
+      IDENTITY_POOL_ID: process.env.REACT_APP_IDENTITY_POOL_ID
     },
   };
   
--- src/containers/Login/Login.js
+++ src/containers/Login/Login.js
@@ -16,6 +16,14 @@ export default function Login() {
     password: ""
   });
 
+  const [newFields, handleNewFieldChange] = useFormFields({
+    code: "",
+    newPassword: "",
+    confirmNewPassword: ""
+  })
+  const [forgotPassword, setForgotPassword] = useState(false);
+  const [newPassword, setNewPassword] = useState(false);
+
   function validateForm() {
     return fields.email.length > 0 && fields.password.length > 0;
   }
@@ -35,21 +43,117 @@ export default function Login() {
     }
   }
 
+  const handleForgotPassword = () => {
+
+
+
+    const handleNewCode = () => {
+
+      const handleNewPasswordSubmit = (e) => {
+        e.preventDefault();
+        const { code, newPassword, confirmNewPassword } = newFields;
+
+        if(newPassword !== confirmNewPassword){
+          alert("Password Fields should match each other.")
+        }
+
+        Auth.forgotPasswordSubmit(fields.email, code, newPassword)
+          .then(data => console.log(data))
+          .catch(err => console.log(err))
+      }
+
+      const { code, newPassword, confirmNewPassword } = newFields
+      return (
+        <div className="Login">
+          <Form onSubmit={handleNewPasswordSubmit}>
+            <Form.Group size="lg">
+              <Form.Label>Enter Code</Form.Label>
+              <Form.Control autoFocus type="text" id="code" value={code} onChange={handleNewFieldChange} />
+            </Form.Group>
+            <Form.Group size="lg">
+              <Form.Label>New Password</Form.Label>
+              <Form.Control autoFocus type="password" id="newPassword" value={newPassword} onChange={handleNewFieldChange} />
+            </Form.Group>
+            <Form.Group size="lg">
+              <Form.Label>Confirm New Password</Form.Label>
+              <Form.Control autoFocus type="password" id="confirmNewPassword" value={confirmNewPassword} onChange={handleNewFieldChange} />
+            </Form.Group>
+            <LoaderButton block size="lg" type="submit" isLoading={isLoading} >
+              Submit
+            </LoaderButton>
+          </Form>
+        </div>
+      )
+    }
+
+    const validateFormForForgotPassword = () => {
+      return fields.email.length > 0;
+    }
+
+    const handleForgotPasswordSubmit = (e) => {
+      e.preventDefault();
+      const { email } = fields
+      Auth.forgotPassword(email)
+        .then(data => {
+          if(data){
+            setNewPassword(true)
+          }
+        })
+        .catch(err => console.log(err))
+    }
+
+    return(
+      <div>
+      {
+        newPassword ? handleNewCode(): (
+          <div className="Login">
+          <Form onSubmit={handleForgotPasswordSubmit}>
+            <Form.Group size="lg" controlId="email">
+              <Form.Label>Email</Form.Label>
+              <Form.Control autoFocus type="email" value={fields.email} onChange={handleFieldChange} />
+            </Form.Group>
+            <LoaderButton block size="lg" type="submit" isLoading={isLoading} disabled={!validateFormForForgotPassword()}>
+              Submit
+            </LoaderButton>
+          </Form>
+        </div>
+        )
+      }
+      </div>
+    )
+  }
+
   return (
-    <div className="Login">
-      <Form onSubmit={handleSubmit}>
-        <Form.Group size="lg" controlId="email">
-          <Form.Label>Email</Form.Label>
-          <Form.Control autoFocus type="email" value={fields.email} onChange={handleFieldChange} />
-        </Form.Group>
-        <Form.Group size="lg" controlId="password">
-          <Form.Label>Password</Form.Label>
-          <Form.Control type="password" value={fields.password} onChange={handleFieldChange}/>
-        </Form.Group>
-        <LoaderButton block size="lg" type="submit" isLoading={isLoading} disabled={!validateForm()}>
-          Login
-        </LoaderButton>
-      </Form>
-    </div>
+    <div>
+    {
+      forgotPassword ? handleForgotPassword() : (
+        <div className="Login">
+          <Form onSubmit={handleSubmit}>
+            <Form.Group size="lg" controlId="email">
+              <Form.Label>Email</Form.Label>
+              <Form.Control autoFocus type="email" value={fields.email} onChange={handleFieldChange} />
+            </Form.Group>
+            <Form.Group size="lg" controlId="password">
+              <Form.Label>Password</Form.Label>
+              <Form.Control type="password" value={fields.password} onChange={handleFieldChange}/>
+            </Form.Group>
+            <LoaderButton block size="lg" type="submit" isLoading={isLoading} disabled={!validateForm()}>
+              Login
+            </LoaderButton>
+            <span style={{cursor: "pointer", fontSize: "0.8rem"}} onClick={() => {
+            setForgotPassword(true);
+            handleForgotPassword();
+            }}>Forgot Password ?</span>
+          </Form>
+
+          <div>
+            <h3>Test Credentials</h3>
+            <h5>user@example.com</h5>
+            <h5>Passw0rd123!</h5>
+          </div>
+        </div>
+      )
+    }
+  </div>
   );
 }
